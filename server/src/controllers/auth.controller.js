635637b52a09d5331ffc6f3f8aca3388de08bdfe
xrafidx <rafidwahyua@gmail.com>
2025-07-24 22:43:02 +0700

    penambahan domain pada cookie

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -12,7 +12,8 @@ export async function login(req, res, next) {
       secure: true, //process.env.NODE_ENV === "production", // Hanya secure di production
       sameSite: "None",
       maxAge: 2 * 24 * 60 * 60 * 1000, // 2 hariSSSSS
-      path: '/'
+      path: '/',
+      domain: '.beabot.vercel.app'
     });
 
     res.status(200).json({
