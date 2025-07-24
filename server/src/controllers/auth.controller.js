fdb50e3b3083fef84d59169f5fc09c22a6f91e6c
xrafidx <rafidwahyua@gmail.com>
2025-07-24 22:24:26 +0700

    update settingan cookie nambahin path /

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -12,6 +12,7 @@ export async function login(req, res, next) {
       secure: true, //process.env.NODE_ENV === "production", // Hanya secure di production
       sameSite: "None",
       maxAge: 2 * 24 * 60 * 60 * 1000, // 2 hariSSSSS
+      path: '/'
     });
 
     res.status(200).json({
