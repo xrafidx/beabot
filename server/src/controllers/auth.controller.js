5492bfa3fe860262332339555cd5ec90011ff81b
Prima Yudhistira <primayudhistira040@gmail.com>
2025-07-25 17:01:23 +0700

    Update auth.controller.js

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -13,7 +13,7 @@ export async function login(req, res, next) {
       sameSite: "None",
       maxAge: 2 * 24 * 60 * 60 * 1000, // 2 hariSSSSS
       path: '/',
-      domain: '.beabot.vercel.app'
+      domain: 'beabot-fe.vercel.app'
     });
 
     res.status(200).json({
