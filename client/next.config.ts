7b6769d5f84bc5ffd5f98766a993150fe89c7aa0
Prima Yudhistira <primayudhistira040@gmail.com>
2025-07-25 20:05:39 +0700

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`, // Proxy ke backend
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
